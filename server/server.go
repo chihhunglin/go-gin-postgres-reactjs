5144739bd4d8f43cca001eb2f99eb0c6fc60d1f1
CH <dragon270329@gmail.com>
2022-11-15 12:02:59 +0800

    go get -u github.com/gin-gonic/gin go get -u github.com/gin-contrib/cors go get -u github.com/gin-gonic/contrib/static

--- server/server.go
+++ server/server.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"go-gin-postgres-reactjs/db"
-	"go-gin-postgres-reactjs/web"
+	// "go-gin-postgres-reactjs/web"
 	"database/sql"
 	_ "github.com/lib/pq"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
 )
+
+type App struct {
+	d db.DB
+}
 
 func main() {
 	d, err := sql.Open("postgres", dataSource())
@@ -15,11 +24,26 @@ func main() {
 		log.Fatal(err)
 	}
 	defer d.Close()
+	app := App{
+		d: db.NewDB(d),
+	}
+	technologies, err := app.d.GetTechnologies()
 	// CORS is enabled only in prod profile
-	cors := os.Getenv("profile") == "prod"
-	app := web.NewApp(db.NewDB(d), cors)
-	err = app.Serve()
-	log.Println("Error", err)
+	prod := os.Getenv("profile") == "prod"
+	r := gin.Default()
+	if !prod {
+		r.Use(cors.Default())
+	}
+	// app := web.NewApp(db.NewDB(d), prod)
+	// err = app.Serve()
+	// log.Println("Error", err)
+	r.Use(static.Serve("/", static.LocalFile("/webapp", true)))
+	r.GET("/api/technologies", func(c *gin.Context) {
+		log.Println(technologies)
+		c.JSON(http.StatusOK, technologies)
+	})
+	log.Println("Web server is available on port 8080")
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func dataSource() string {
